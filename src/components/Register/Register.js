106398545b08cb5ca4cde35e32f627b7282136d6
mikesinc <mas.sinclair@gmail.com>
2019-02-02 18:05:26 +0800

    Remove build

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register', {
+        fetch('https://safe-atoll-17746.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
