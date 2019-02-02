5c3a54cdafc29465a61c33c036d63492da2d1783
mikesinc <mas.sinclair@gmail.com>
2019-02-02 23:47:02 +0800

    initial

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register', {
+        fetch('https://shrouded-journey-20606.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
